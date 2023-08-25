8431812919d824d748f1711f29e1e008397df5f1
Qscad <ilyavph@proton.me>
2023-08-25 13:06:25 +0000

    perf(dedupe): :zap: improve hash serialization

--- src/collection_keeper/dedupe/images.py
+++ src/collection_keeper/dedupe/images.py
@@ -1,11 +1,12 @@
 """Dedupe images."""
 import os
 from glob import glob
+from sqlite3 import Binary
 from typing import Generator
 
-from imagehash import phash
+from imagehash import ImageHash, hex_to_hash, phash
 from PIL import Image
-from sqlitedict import SqliteDict
+from sqlitedict import SqliteDict, decode, encode
 
 from collection_keeper.config import Config
 
@@ -14,6 +15,16 @@ def is_image(filename: str) -> bool:
     """Check if the file has image extension."""
     f = filename.lower()
     return f.endswith((".png", ".jpg", ".jpeg", ".bmp", ".gif", ".svg")) or ".jpg" in f
+
+
+def encode_hash(image_hash: ImageHash) -> Binary:
+    """Convert image hash to hex string."""
+    return encode(str(image_hash))
+
+
+def decode_hash(stored_data: Binary) -> ImageHash:
+    """Convert hex string to image hash."""
+    return hex_to_hash(decode(stored_data))
 
 
 def update_phashes(root_folder: str) -> None:
@@ -25,7 +36,7 @@ def update_phashes(root_folder: str) -> None:
     db_path = Config.get("postprocessing").get("dedupe_strategy").get("phashes_db")
     commit_freq = Config.get("postprocessing").get("dedupe_strategy").get("commit_freq")
     calculated_phashes = 0
-    with SqliteDict(db_path) as db:
+    with get_dict(db_path) as db:
         for img in generate_image_paths(root_folder):
             if img not in db:
                 img_phash = phash(Image.open(img))
@@ -34,6 +45,11 @@ def update_phashes(root_folder: str) -> None:
                 if (calculated_phashes + 1) % commit_freq == 0:
                     db.commit()
         db.commit()
+
+
+def get_dict(db_path: str) -> SqliteDict:
+    """Get sqlite dictionary to store hashes."""
+    return SqliteDict(db_path, encode=encode_hash, decode=decode_hash)
 
 
 def generate_image_paths(root_folder: str) -> Generator[str, None, None]:
--- tests/test_dedup.py
+++ tests/test_dedup.py
@@ -1,7 +1,9 @@
 """Test all things dedup."""
 import os
+import tempfile
 
-from sqlitedict import SqliteDict
+from imagehash import phash
+from PIL import Image
 
 from collection_keeper import config
 from collection_keeper.dedupe import images
@@ -15,15 +17,24 @@ def test_is_image() -> None:
     assert images.is_image("a.gif")
 
 
+def test_hash_serialization() -> None:
+    """Test encode_hash and decode_hash."""
+    hsh = phash(Image.open("./test_images/a/cat879.jpg"))
+    assert images.decode_hash(images.encode_hash(hsh)) == hsh
+
+
 def test_update_phashes() -> None:
     """Test update_phashes."""
     config.Config.set_config_path(os.path.abspath("./test_collection.yml"))
-    images.update_phashes("./test_images/")
-    db_path = (
-        config.Config.get("postprocessing").get("dedupe_strategy").get("phashes_db")
-    )
-    with SqliteDict(db_path) as db:
-        assert len(db) == 6
-        assert db["./test_images/a/cat879.jpg"] == db["./test_images/c/cat879.jpg"]
-        assert db["./test_images/a/cat879.jpg"] != db["./test_images/c/cat4270.jpg"]
-    os.remove(db_path)
+    with tempfile.NamedTemporaryFile() as tmp_file:
+        config.Config._config["postprocessing"]["dedupe_strategy"][
+            "phashes_db"
+        ] = tmp_file.name
+        images.update_phashes("./test_images/")
+        db_path = (
+            config.Config.get("postprocessing").get("dedupe_strategy").get("phashes_db")
+        )
+        with images.get_dict(db_path) as db:
+            assert len(db) == 6
+            assert db["./test_images/a/cat879.jpg"] == db["./test_images/c/cat879.jpg"]
+            assert db["./test_images/a/cat879.jpg"] != db["./test_images/c/cat4270.jpg"]
