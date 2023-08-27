65539b03e324eb1151db7e3ddfb854f9ba402764
Qscad <ilyavph@proton.me>
2023-08-27 14:31:56 +0000

    fix(dev): :white_check_mark: fix test_download_tags

--- tests/test_download.py
+++ tests/test_download.py
@@ -17,5 +17,6 @@ def test_download_tags(tmpdir, mocker) -> None:
     with tmpdir.as_cwd() as _:
         paths, errors = download.download_tags()
         assert len(paths) == 1
-        assert len(errors) == 0
+        assert len(errors) == 1
+        assert len(errors[0]) == 0
         assert "tag_1" in paths[0]
